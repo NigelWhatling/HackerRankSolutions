1a2a6698dfa95898f903a0e1ef8103b8cd61ca50
Nigel Whatling <nigelw@n-vision.com.au>
2019-10-10 14:50:47 +1000

    TestUtil update.

--- src/TestUtils/TestUtil.cs
+++ src/TestUtils/TestUtil.cs
@@ -4,7 +4,7 @@
     using System.IO;
     using NUnit.Framework;
 
-    public class TestUtil
+    public static class TestUtil
     {
         /// <summary>Runs the specified action using the provided input data.</summary>
         /// <param name="input">The input to be injected to the console stdin.</param>
@@ -12,20 +12,36 @@
         /// <param name="keepOutput"></param>
         public static string Run(string[] input, Action action, bool keepOutput = false)
         {
-            string outputPath = Path.Combine(TestContext.CurrentContext.WorkDirectory, $"{TestContext.CurrentContext.Test.ClassName} - {TestContext.CurrentContext.Test.Name}.txt");
-            TestContext.WriteLine($"Test '{TestContext.CurrentContext.Test.Name}': {outputPath}");
+            input = input ?? throw new ArgumentNullException(nameof(input));
+            action = action ?? throw new ArgumentNullException(nameof(action));
+
+            string outputFilename = $"{TestContext.CurrentContext.Test.ClassName}__{TestContext.CurrentContext.Test.Name}"
+                .Replace('.', '_')
+                .Replace(' ', '_');
+
+            string fileOutputPath = Path.Combine(TestContext.CurrentContext.WorkDirectory, outputFilename + ".out.txt");
+            string consoleOutputPath = Path.Combine(TestContext.CurrentContext.WorkDirectory, outputFilename + ".con.txt");
+
+            TestContext.WriteLine($"File output: {fileOutputPath}");
+            TestContext.WriteLine($"Console output: {consoleOutputPath}");
 
             // Delete existing output files.
-            if (File.Exists(outputPath))
+            if (File.Exists(fileOutputPath))
             {
-                File.Delete(outputPath);
+                File.Delete(fileOutputPath);
             }
 
             // Set the OUTPUT_PATH environment variable which is used by the standard HackerRank code.
-            Environment.SetEnvironmentVariable("OUTPUT_PATH", outputPath);
+            Environment.SetEnvironmentVariable("OUTPUT_PATH", fileOutputPath);
 
-            using (MemoryStream ms = new MemoryStream())
+            FileStream fs = null;
+            MemoryStream ms = null;
+
+            try
             {
+                fs = File.Open(consoleOutputPath, FileMode.Create, FileAccess.Write, FileShare.Write);
+                ms = new MemoryStream();
+
                 using (StreamWriter sw = new StreamWriter(ms))
                 {
                     foreach (string line in input)
@@ -36,20 +52,47 @@
                     sw.Flush();
                     ms.Seek(0, SeekOrigin.Begin);
 
+                    using (StreamWriter sw2 = new StreamWriter(fs))
                     using (StreamReader sr = new StreamReader(ms))
                     {
+                        Console.SetOut(sw2);
                         Console.SetIn(sr);
                         action();
                     }
                 }
             }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Dispose();
+                }
 
-            string result = File.ReadAllText(outputPath).Trim();
+                if (ms != null)
+                {
+                    ms.Dispose();
+                }
+            }
+
+            string result = null;
+
+            if (File.Exists(fileOutputPath))
+            {
+                result = File.ReadAllText(fileOutputPath).Trim();
+            }
+            else if (File.Exists(consoleOutputPath))
+            {
+                result = File.ReadAllText(consoleOutputPath).Trim();
+            }
 
             if (!keepOutput)
             {
-                File.Delete(outputPath);
+                File.Delete(fileOutputPath);
+                File.Delete(consoleOutputPath);
             }
+
+            TestContext.WriteLine("Result:");
+            TestContext.WriteLine(result);
 
             return result;
         }
